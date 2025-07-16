a9fbe5f9f4eafb7d382ac15ca03cf05e54155bbe
mleung2019 <50841395+mleung2019@users.noreply.github.com>
2025-07-16 16:40:16 -0700

    Use metadata for extension path

--- extension.js
+++ extension.js
@@ -14,40 +14,33 @@ const KEYBINDINGS = [
 let settings;
 
 function moveWindow([xLeft, yTop], [width, height]) {
-  try {
-    let win = global.display.get_focus_window();
-    if (!win) {
-      global.log("No focused window");
-      return;
-    }
+  let win = global.display.get_focus_window();
+  if (!win) return;
 
-    win.unmaximize(Meta.MaximizeFlags.BOTH);
+  // Unmaximize window before snapping
+  win.unmaximize(Meta.MaximizeFlags.BOTH);
 
-    let monitorIndex = win.get_monitor();
-    let workArea = Main.layoutManager.monitors[monitorIndex];
+  let monitorIndex = win.get_monitor();
+  let workArea = Main.layoutManager.monitors[monitorIndex];
 
-    const newX = workArea.x + workArea.width * xLeft;
-    const newY = workArea.y + workArea.height * yTop;
-    const newW = workArea.width * width;
-    const newH = workArea.height * height;
+  const newX = workArea.x + workArea.width * xLeft;
+  const newY = workArea.y + workArea.height * yTop;
+  const newW = workArea.width * width;
+  const newH = workArea.height * height;
 
-    global.log(`Moving window to (${newX}, ${newY}) size (${newW}, ${newH})`);
-    win.move_resize_frame(false, newX, newY, newW, newH);
-  } catch (err) {
-    global.log(err);
-  }
+  win.move_resize_frame(false, newX, newY, newW, newH);
 }
 
-function init() {
+function init(metadata) {
+  const extensionPath = metadata.path;
+
   const schemaSource = Gio.SettingsSchemaSource.new_from_directory(
-    "/home/mint/.local/share/cinnamon/extensions/mopsys-window-snapper@mleung2019/schemas",
+    `${extensionPath}/schemas`,
     Gio.SettingsSchemaSource.get_default(),
     false
   );
-  const schema = schemaSource.lookup(
-    "org.mleung2019.mopsys-window-snapper",
-    true
-  );
+
+  const schema = schemaSource.lookup("org.mleung2019.window-snapper", true);
   settings = new Gio.Settings({ settings_schema: schema });
 }
 
@@ -58,7 +51,6 @@ function enable() {
       settings,
       Meta.KeyBindingFlags.NONE,
       () => {
-        global.log(keybind.name);
         moveWindow(keybind.topLeft, keybind.size);
       }
     );
